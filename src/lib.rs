e57d0eb6d81c4ed3d92b60503412dab4251646ad
Tim Harding <Tim@TimHarding.co>
2025-01-17 17:20:20 -0800

    Parsing integers

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,46 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use nom::{
+    branch::alt,
+    character::streaming::{char, digit0},
+    combinator::{map, opt, value, verify},
+    sequence::{delimited, tuple},
+    IResult, Needed,
+};
+
+fn int(i: &[u8]) -> IResult<&[u8], i64> {
+    delimited(char('i'), alt((zero, nonzero)), char('e'))(i)
+}
+
+fn nonzero(i: &[u8]) -> IResult<&[u8], i64> {
+    map(nonzero_raw, |(minus, head, tail)| {
+        let mut out = (head - b'0') as i64;
+        for c in tail {
+            out = out * 10 + (c - b'0') as i64;
+        }
+        out * if minus { -1 } else { 1 }
+    })(i)
+}
+
+fn nonzero_raw(i: &[u8]) -> IResult<&[u8], (bool, u8, &[u8])> {
+    tuple((minus, verify(byte, is_nonzero), digit0))(i)
+}
+
+fn minus(i: &[u8]) -> IResult<&[u8], bool> {
+    map(opt(char('-')), |minus| minus.is_some())(i)
+}
+
+fn zero(i: &[u8]) -> IResult<&[u8], i64> {
+    value(0, char('0'))(i)
+}
+
+fn is_nonzero(&b: &u8) -> bool {
+    b >= b'1' && b <= b'9'
+}
+
+fn byte(i: &[u8]) -> IResult<&[u8], u8> {
+    match i {
+        [head, tail @ ..] => Ok((tail, *head)),
+        [] => Err(nom::Err::Incomplete(Needed::new(1))),
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +48,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn valid_ints() {
+        assert_eq!(int(b"i0e"), Ok((&[][..], 0)));
+        assert_eq!(int(b"i115e"), Ok((&[][..], 115)));
+        assert_eq!(int(b"i-12e"), Ok((&[][..], -12)));
     }
 }
